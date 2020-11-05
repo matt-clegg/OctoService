22e04d791dd6b1cb457c5320ff03bb01be01ed3d
Matt <mattclegg93@gmail.com>
2020-11-05 07:48:40 +0000

    Another change

--- OctoService/Worker.cs
+++ OctoService/Worker.cs
@@ -21,7 +21,7 @@ namespace OctoService
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Amazing test worker running at: {time}", DateTimeOffset.Now);
+                _logger.LogInformation("Testing worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
         }
